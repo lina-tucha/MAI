34430a99e3ad5e61c1257e08d0f08b108e92d97c
lina-tucha <71285415+lina-tucha@users.noreply.github.com>
2021-01-22 22:38:35 +0300

    Update J.cpp

--- 1 course/Olympiad programming/Contest 1 (C++)/J.cpp
+++ 1 course/Olympiad programming/Contest 1 (C++)/J.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int main() {
-	int a, b; 
+    int a, b; 
     long long k = 0;
-	cin >> a >> b;
-	if (a < b) {
+    cin >> a >> b;
+    if (a < b) {
         for (int i = a; i <= b; i++)
-	        k = k + i;
+	    k = k + i;
         cout << k;
     } else {
-	    for (int i = b; i <= a; i++)
-	        k = k + i;
+	for (int i = b; i <= a; i++)
+	    k = k + i;
         cout << k;
     }
-	return 0; 
+    return 0; 
 }
