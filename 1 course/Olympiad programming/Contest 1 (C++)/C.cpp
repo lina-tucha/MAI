1bedb7ae1cb0d43348e1cb30391cc507fbc3655f
lina-tucha <71285415+lina-tucha@users.noreply.github.com>
2021-01-22 22:40:11 +0300

    Update C.cpp

--- 1 course/Olympiad programming/Contest 1 (C++)/C.cpp
+++ 1 course/Olympiad programming/Contest 1 (C++)/C.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main() {
-	int a, b, c;
-	cin >> a >> b >> c;
-	if ((a >= b) && (a >= c)) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if ((a >= b) && (a >= c)) {
         cout << a;
     } else if ((b >= c) && (b >= a)) { 
         cout << b;
     } else cout << c;
-	return 0; 
+    return 0; 
 }
